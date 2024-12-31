76371b98b5e414bdfb6b4e7d6b77bc714af93ed2
MRK <135884817+Rkhan2026@users.noreply.github.com>
2024-12-31 12:58:41 +0530

    added create user in index.js

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,25 @@ const prisma = new PrismaClient();// Initialize Prisma Client to interact with t
 app.use(express.json());
 
 // API Endpoints ---> Test endpoint to check if the API is working
-app.get('/test', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         res.status(200).json({ message: 'API Is Working' });
     } catch (error) {
         res.status(500).json({ message: error.message }); // Handle errors and send a 500 status
     }
 });
+
+// Create a new user
+app.post('/users', async (req, res) => {
+    try {
+        const userData = req.body; // Get user data from the request body
+        const newUser = await prisma.user.create({ data: userData }); // Create the user in the database
+        res.status(201).json(newUser); // Send 201 Created status
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 // Start the server
 const PORT = process.env.PORT || 4000; // Use the environment port or 4000 as a default
